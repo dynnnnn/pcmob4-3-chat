2b73080ed4f1b3ffb8e9c1690fb384fc2e27aedc
gladyn <gladynwong@gmail.com>
2022-06-15 12:09:19 +0800

    add screen

---
+++ screens/ChatScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stylesheet, Text, View } from "react-native";
+
+export default function ChatScreen(){
+    return(
+        <View>
+            <Text>Chatscreen</Text>
+        </View>
+    );
+}
---
+++ screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stylesheet, Text, View } from "react-native";
+
+export default function LoginScreen(){
+    return(
+        <View>
+            <Text>Loginscreen</Text>
+        </View>
+    );
+}
